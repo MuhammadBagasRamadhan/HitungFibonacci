5086e212719f507306d7a05526f705170745d1b3
MhVagsNs <MhVagsNs@User.mshome.net>
2018-09-13 12:54:13 -0800

    Initial Commit

---
+++ src/Fibonacci/Latihan01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fibonacci;
+
+/**
+ *
+ * @author MhVagsNs
+ */
+public class Latihan01 {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        
+    }
+    
+}
